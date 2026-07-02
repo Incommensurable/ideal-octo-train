2e81e741b0846a913ba8a1ee28ad3dd9172cc9b6
hjh <h.j.halcro@gmail.com>
2026-07-02 16:01:36 -0400

    our first python lesson :) It tells you the linked articles from a given wikipedia article

---
+++ main.py
@@ -0,0 +1,25 @@
+#to run the venv in terminal: "source venv/bin/activate"
+
+import requests
+import re
+
+def format_undersc(text):
+    text_words = text.split(" ")
+    return "_".join(text_words)
+
+def get_article(title):
+    title_argument = format_undersc(title)
+    url = f"https://en.wikipedia.org/w/index.php?title={title_argument}&action=raw" 
+    response = requests.get(url, headers={"user-agent":"curl/8.5.0"})
+    return response.text
+
+def get_links(article_text):
+    word_pattern = re.compile(r"\[\[([\w\s]+)\]\]")
+    list_links = word_pattern.findall(article_text)
+    return list_links
+
+
+x = input("prompt: ")
+wiki_mush = get_article(x)
+print(sorted(get_links(wiki_mush)))
+print(f"https://en.wikipedia.org/wiki/{format_undersc(x)}")
